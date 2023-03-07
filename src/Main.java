5f1770884e863375404d79a038f051a66fda6d21
Anton <toniq133@bk.ru>
2023-03-07 14:09:40 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 333;
+        int amount = 10000;
         int a = 20;
         int milli = amount / a;
         System.out.println(milli);
