489a91d4a091dd4226e895c5bd155b1acb0985c4
Anton <toniq133@bk.ru>
2023-03-07 14:03:49 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int amount = 333;
+        int a = 20;
+        int milli = amount / a;
+        System.out.println(milli);
+
+
     }
 }
