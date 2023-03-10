2aca299664636db999e04cbb12e1394fa6cf826d
Anton <toniq133@bk.ru>
2023-03-10 10:57:19 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 10000;
-        int a = 20;
-        int milli = amount / a;
-        System.out.println(milli);
+        int stake = 20;
+        int mile = amount / stake;
+        System.out.println(mile);
 
 
     }
